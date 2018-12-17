63d234ac8c073a6d81a96af2ceaf30dc09c6d9dc
Amrit <samritpal313@gmail.com>
2018-12-17 15:34:39 +0000

    Completed restrictions on UI

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   resources :users, except:[:new]
   
   resources :articles
+  
+  get 'login',to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout',to:'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".   
   # You can have the root of your site routed with "root"
